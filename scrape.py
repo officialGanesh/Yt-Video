0e705c47e72d364cec952c0919763e9c22842e03
Ganesh <ganuofficial2795@gmail.com>
2021-06-10 23:32:16 +0530

    Youtube vid

--- scrape.py
+++ scrape.py
@@ -1,6 +1,6 @@
-from selenium import webdriver
 import asyncio
-
+from selenium import webdriver
+from time import sleep
 
 PATH = 'C:\Program Files (x86)\geckodriver.exe'
 
@@ -10,8 +10,17 @@ async def main():
     url = 'https://www.youtube.com'
     driver = webdriver.Firefox(executable_path=PATH)
     driver.get(url)
-
-
+    
+    yt_channel = 'youtube channel name'     
+    
+    query = driver.find_element_by_name('search_query')
+    query.send_keys(yt_channel)
+    
+    search_btn = driver.find_element_by_id("search-icon-legacy").click()
+    
+    vid = driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-shelf-renderer[1]/div[1]/div[2]/ytd-vertical-list-renderer/div[1]/ytd-video-renderer[1]').click()
+    
+    sleep(120) # max video play time 
 
     driver.quit()
 
